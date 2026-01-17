d031bfb9ccb6fd5d0aa8cfcf9bd79ae361d4d34b
YourNewUsername <mia5w.maple@gmail.com>
2026-01-17 16:20:43 -0500

    1.add Responsive Styles 2.add links 3.add projects

--- main.js
+++ main.js
@@ -32,7 +32,9 @@ fetch('https://yuki-me-backend.onrender.com/api/snow-clicks')
 //fetch('http://127.0.0.1:3000/api/snow-clicks')
   .then(r => r.json())
   .then(data => {
-    updateDisplay(data.count);
+    if (snowNumDisplay) {
+      snowNumDisplay.textContent = data.count;
+    }
   })
   .catch(err => console.error("failed load snow num ", err));
 
@@ -50,8 +52,9 @@ snowBtn.addEventListener('click', () => {
   snowBtn.style.cursor = 'not-allowed';
 
   // 3. send num to backend
-  let currentVal = parseInt(snowNumDisplay.textContent) || 0;
-  updateDisplay(currentVal + 1);
+  let currentVal = parseInt(snowNumDisplay.textContent, 10) || 0;//10 for decimal
+  snowNumDisplay.textContent = currentVal + 1;
+
   sendClicksToServer(1);
 
   // 4. start snow
@@ -389,11 +392,10 @@ pageOlderBtn.addEventListener('click', () => {
   }
 });
 
+
 //last step----------------------------------------
 // wait for all events configure successfully
 loadComments(1);
-
-
 
 
 //========================Helper functions===================
@@ -537,7 +539,7 @@ function createSingleCommentHTML(c, level) {
         </div>
 
         <div class="generate-comment-reply" data-id="${c._id}" data-name="${c.name}">
-          <svg width=30 height=20 viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M6.4 3.80353C7.55322 2.26658 10 3.08182 10 5.00302V8.00928C14.6772 7.86093 17.7771 9.50672 19.7796 11.7657C21.8614 14.1142 22.6633 17.0184 22.9781 18.9028C23.116 19.7283 22.5806 20.3237 22.0149 20.5275C21.4711 20.7234 20.7467 20.6283 20.2749 20.0531C18.6945 18.1261 15.5 15.4884 10 15.4884V18.997C10 20.9182 7.55321 21.7334 6.4 20.1965L1.6 13.7992C0.800001 12.733 0.800001 11.267 1.6 10.2008L6.4 3.80353ZM8 5.00302L3.2 11.4003C2.93333 11.7557 2.93333 12.2443 3.2 12.5997L8 18.997V14.5C8 13.9477 8.44772 13.5 9 13.5H10C17 13.5 20.6009 17.4621 20.6009 17.4621C20.1828 16.0361 19.4749 14.4371 18.2829 13.0924C16.7183 11.3273 14.5 10 10 10H9C8.44772 10 8 9.55228 8 9V5.00302Z" ></path> </g></svg>
+          <svg width=24 height=20 viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M6.4 3.80353C7.55322 2.26658 10 3.08182 10 5.00302V8.00928C14.6772 7.86093 17.7771 9.50672 19.7796 11.7657C21.8614 14.1142 22.6633 17.0184 22.9781 18.9028C23.116 19.7283 22.5806 20.3237 22.0149 20.5275C21.4711 20.7234 20.7467 20.6283 20.2749 20.0531C18.6945 18.1261 15.5 15.4884 10 15.4884V18.997C10 20.9182 7.55321 21.7334 6.4 20.1965L1.6 13.7992C0.800001 12.733 0.800001 11.267 1.6 10.2008L6.4 3.80353ZM8 5.00302L3.2 11.4003C2.93333 11.7557 2.93333 12.2443 3.2 12.5997L8 18.997V14.5C8 13.9477 8.44772 13.5 9 13.5H10C17 13.5 20.6009 17.4621 20.6009 17.4621C20.1828 16.0361 19.4749 14.4371 18.2829 13.0924C16.7183 11.3273 14.5 10 10 10H9C8.44772 10 8 9.55228 8 9V5.00302Z" ></path> </g></svg>
         </div>
       </div>
       <div class="generate-comment-content" >${c.content}</div>
@@ -644,13 +646,6 @@ function createSnow() {
   setTimeout(() => {
     snow.remove();
   }, 10000);//when delete the snow flower div
-}
-
-//??--------
-function updateDisplay(num) {
-  if(snowNumDisplay) {
-      snowNumDisplay.textContent = num.toString().padStart(3, '0');
-  }
 }
 
 function sendClicksToServer(countToAdd) {
